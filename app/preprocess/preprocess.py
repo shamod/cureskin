eaa1aff84b0d444119f468c1e71e55df422b7ae2
haggishm <30947652+haggishm@users.noreply.github.com>
2019-09-26 08:23:55 +1200

    Create preprocess.py

---
+++ app/preprocess/preprocess.py
@@ -0,0 +1,38 @@
+  GNU nano 2.9.3                                       main.py                                                  
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+from glob import glob
+import seaborn as sns
+base_skin_dir = os.path.join('.', 'datasets')
+imageid_path_dict = {os.path.splitext(os.path.basename(x))[0]: x
+                     for x in glob(os.path.join(base_skin_dir, '*', '*.jpg'))}
+
+lesion_type_dict = {
+    'nv': 'Melanocytic nevi',
+    'mel': 'melanoma',
+    'bkl': 'Benign keratosis-like lesions ',
+    'bcc': 'Basal cell carcinoma',
+    'akiec': 'Actinic keratoses',
+    'vasc': 'Vascular lesions',
+    'df': 'Dermatofibroma'
+}
+tile_df = pd.read_csv(os.path.join(base_skin_dir, 'HAM10000_metadata.csv'))
+tile_df['path'] = tile_df['image_id'].map(imageid_path_dict.get)
+tile_df['cell_type'] = tile_df['dx'].map(lesion_type_dict.get) 
+tile_df['cell_type_idx'] = pd.Categorical(tile_df['cell_type']).codes
+tile_df.sample(3)
+
+tile_df.describe(exclude=[np.number])
+
+
+fig, ax1 = plt.subplots(1, 1, figsize = (10, 5))
+tile_df['cell_type'].value_counts().plot(kind='bar', ax=ax1)
+
+# load in all of the images
+from skimage.io import imread
+tile_df['image'] = tile_df['path'].map(imread)
+
+                                    
