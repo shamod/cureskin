c00bd92b269fba81c865b04998625cf36258c5a0
dgildeh <dgildeh@davidgildeh.com>
2019-10-07 23:45:29 -0700

    Tidied up files, added logo images and initial prediction model to fill out

--- model/preprocess/preprocess.py
+++ model/preprocess/preprocess.py
@@ -1,5 +1,3 @@
-  GNU nano 2.9.3                                       main.py                                                  
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
